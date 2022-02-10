80f40bb83d583469193b0ca7532579f3fa75d3de
chrismenke45 <chrismenke45@gmail.com>
2022-02-09 17:34:24 -0800

    initial commit

---
+++ models/message.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema(
+  {
+    message: {type: String, required: true, maxLength: 100},
+    message_date: {type: Date, default: Date.now}
+  }
+);
+
+// Virtual for preview of message
+MessageSchema
+.virtual('messageStart')
+.get(function () {
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+  var fullMessage = this.message;
+  messageStart = fullMessage.substring(0,14)
+  return messageStart;
+});
+
+
+// Virtual for author's URL
+MessageSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/message/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Message', MessageSchema);
